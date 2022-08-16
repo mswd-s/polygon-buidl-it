ca6666a102443672e877bf35ff11f21ceddd67ec
mswd-s <code.maswood@gmail.com>
2022-08-17 04:12:55 +0530

    polygon-mumbai-project-to-do

---
+++ server/hardhat.config.js
@@ -0,0 +1,27 @@
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
+module.exports = {
+   solidity: "0.8.4",
+   defaultNetwork: "polygon_mumbai",
+   networks: {
+      hardhat: {},
+      polygon_mumbai: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
+      }
+   },
+}
